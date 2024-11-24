6b57228c9d8bfcb6f8c6f375cc4d4f216ba25bed
yatakeke <yatasunshine.1456@gmail.com>
2024-11-24 22:32:09 +0900

    三角測量を１テストで行う

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -6,14 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
 
     @Test
-    @DisplayName("1のコストを持つカードで相手を攻撃すると相手のライフが2から1に減る")
+    @DisplayName("1のコストを持つカードで相手を攻撃すると相手のライフを1減らせる")
     public void playerCanAttackBy1CostCard() {
         var active = new Player(2);
         var opponent = new Player(2);
-
         var card = new Card();
-        active.attack(card, opponent);
 
+        active.attack(card, opponent);
         assertEquals(1, opponent.life());
+
+        active.attack(card, opponent);
+        assertEquals(0, opponent.life());
+
     }
 }
