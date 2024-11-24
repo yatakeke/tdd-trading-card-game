8c11734e24985b3f8e8381feee740f8e2206a4bc
yatakeke <yatasunshine.1456@gmail.com>
2024-11-24 22:41:16 +0900

    nested classで囲った

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,32 +1,36 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
-    @Test
-    @DisplayName("1のコストを持つカードで相手を攻撃すると相手のライフを1減らせる")
-    public void playerCanAttackBy1CostCard() {
-        var active = new Player(2);
-        var opponent = new Player(2);
-        var card = new Card(1);
+    @Nested
+    class AttackTest {
+        @Test
+        @DisplayName("1のコストを持つカードで相手を攻撃すると相手のライフを1減らせる")
+        public void playerCanAttackBy1CostCard() {
+            var active = new Player(2);
+            var opponent = new Player(2);
+            var card = new Card(1);
 
-        active.attack(card, opponent);
-        assertEquals(1, opponent.life());
+            active.attack(card, opponent);
+            assertEquals(1, opponent.life());
 
-        active.attack(card, opponent);
-        assertEquals(0, opponent.life());
-    }
+            active.attack(card, opponent);
+            assertEquals(0, opponent.life());
+        }
 
-    @Test
-    @DisplayName("2のコストを持つカードで相手を攻撃すると相手のライフを2減らせる")
-    public void playerCanAttackBy2CostCard() {
-        var active = new Player(2);
-        var opponent = new Player(2);
-        var card = new Card(2);
+        @Test
+        @DisplayName("2のコストを持つカードで相手を攻撃すると相手のライフを2減らせる")
+        public void playerCanAttackBy2CostCard() {
+            var active = new Player(2);
+            var opponent = new Player(2);
+            var card = new Card(2);
 
-        active.attack(card, opponent);
-        assertEquals(0, opponent.life());
+            active.attack(card, opponent);
+            assertEquals(0, opponent.life());
+        }
     }
 }
